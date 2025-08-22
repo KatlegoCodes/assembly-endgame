7fde4eff194580d06b87cece3b748f973a22bff5
Frost <moosemashegona@gmail.com>
2025-08-22 13:41:54 +0200

    Update common header component

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <div className="container">
+      <header>
         <h1>Assembly:Endgame</h1>
         <p>
           Guess the word in under 8 minutes to keep the programming world safe
           from assembly
         </p>
-      </div>
+      </header>
     </div>
   );
 };
