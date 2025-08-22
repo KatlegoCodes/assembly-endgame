9dfc4bbacb7ea0b567f7b510a01aef66005696bb
Frost <moosemashegona@gmail.com>
2025-08-22 12:45:24 +0200

    Update homepage layout

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="container">
+        <h1>Assembly:Endgame</h1>
+        <p>
+          Guess the word in under 8 minutes to keep the programming world safe
+          from assembly
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
