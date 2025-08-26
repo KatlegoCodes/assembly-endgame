421b46b6b149474203363854c05f3d66ce338ff2
Frost <moosemashegona@gmail.com>
2025-08-26 18:18:13 +0200

    removed header and body component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import React from "react";
-import Header from "./Components/Header/Header";
-import "./index.css";
-import Body from "./Components/Body";
+import { useState } from "react";
+import { languages } from "./languages";
 
-const App = () => {
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    };
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    );
+  });
+
+  const letterElements = currentWord
+    .split("")
+    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+
+  const keyboardElements = alphabet
+    .split("")
+    .map((letter) => <button key={letter}>{letter.toUpperCase()}</button>);
+
   return (
-    <div>
-      <Header />
-      <Body />
-    </div>
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </header>
+      <section className="game-status">
+        <h2>You win!</h2>
+        <p>Well done! 🎉</p>
+      </section>
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">{letterElements}</section>
+      <section className="keyboard">{keyboardElements}</section>
+      <button className="new-game">New Game</button>
+    </main>
   );
-};
-
-export default App;
+}
