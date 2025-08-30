51bc461536ddc68bce99f2b5526a436a26eb2f31
Frost <moosemashegona@gmail.com>
2025-08-30 15:22:49 +0200

    Added game logic to allow users to guess letters and display them

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,11 @@ export default function AssemblyEndgame() {
 
   const letterElements = currentWord
     .split("")
-    .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
+    .map((letter, index) => (
+      <span key={index}>
+        {guessedLetters.includes(letter) ? letter.toUpperCase() : ""}
+      </span>
+    ));
 
   const keyboardElements = alphabet.split("").map((letter) => {
     const isGuessed = guessedLetters.includes(letter);
