923e18b99e5fdbf91fa966a75433b0b0827e9679
Frost <moosemashegona@gmail.com>
2025-09-06 15:21:10 +0200

    Refactored the conditional rendering function with a helper function

--- src/App.jsx
+++ src/App.jsx
@@ -80,6 +80,27 @@ export default function AssemblyEndgame() {
     lost: isGameLost,
   });
 
+  const renderGameStatus = () => {
+    if (!isGameOver) {
+      return null;
+    }
+    if (isGameWon) {
+      return (
+        <>
+          <h2>You Win</h2>
+          <p>Well done! 🎉</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2>You Lose</h2>
+          <p>Better luck next time! 😢</p>
+        </>
+      );
+    }
+  };
+
   return (
     <main>
       <header>
@@ -89,21 +110,7 @@ export default function AssemblyEndgame() {
           from Assembly!
         </p>
       </header>
-      <section className={gameStatusClass}>
-        {isGameOver ? (
-          isGameWon ? (
-            <>
-              <h2>You win!</h2>
-              <p>Well done! 🎉</p>
-            </>
-          ) : (
-            <>
-              <h2>Game over!</h2>
-              <p>You lose! Better start learning Assembly 😭</p>
-            </>
-          )
-        ) : null}
-      </section>
+      <section className={gameStatusClass}>{renderGameStatus()}</section>
       <section className="language-chips">{languageElements}</section>
       <section className="word">{letterElements}</section>
       <section className="keyboard">{keyboardElements}</section>
